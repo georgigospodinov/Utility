3512990481a1870ee7090223ccc3c0e9be1cb63a
Georgi Gospodinov <georgigospodinov@gmail.com>
2019-02-14 15:47:06 +0000

    Applied Sun Code Conventions to PrintFormatting.

--- java/util/PrintFormatting.java
+++ java/util/PrintFormatting.java
@@ -1,33 +1,65 @@
 package util;
 
-import sun.security.action.*;
+import sun.security.action.GetPropertyAction;
 
-import java.security.*;
-import java.util.*;
+import java.security.AccessController;
+import java.util.Collection;
 
 /**
  * Provides a method to "pretty-print" multiple objects.
  *
  * @version 2.1
  */
-public class PrintFormatting {
+public final class PrintFormatting {
 
-    public static final String NEW_LINE = AccessController.doPrivileged(new GetPropertyAction("line.separator"));
+    /** A reference to the system-specific line separator. */
+    public static final String NEW_LINE = AccessController.doPrivileged(
+            new GetPropertyAction("line.separator"));
+
+    /** A constant String reference ot a comma. */
     public static final String SEPARATOR = ",";
 
-    public static void print(Object... objects) {
+    /** Hides the default constructor for this utility class. */
+    private PrintFormatting() {
+    }
+
+    /**
+     * Prints all objects to standard output.
+     * Each object is printed on a separate line.
+     * If a given object is an array, its elements are printed on separately
+     * and are indented with tabs.
+     * Elements of nested arrays are indented multiple times.
+     *
+     * @param objects the objects to print
+     */
+    public static void print(final Object... objects) {
         for (Object o : objects) {
             print(o, 0);
         }
     }
 
-    private static void printTabs(int numberOfTabs) {
+    /**
+     * Prints the specified number of tab characters to standard output.
+     * This method is used to indent the elements of arrays that are to be
+     * printed.
+     *
+     * @param numberOfTabs the number of tabs to print
+     */
+    private static void printTabs(final int numberOfTabs) {
         for (int i = 0; i < numberOfTabs; i++) {
             System.out.print("\t");
         }
     }
 
-    private static void printArray(Object[] arr, int nestingLevel) {
+    /**
+     * Prints the given array to standard output.
+     * Each element is indented with
+     * a number of tabs equal to the nesting level.
+     *
+     * @param arr          the array whose elements are to be printed
+     * @param nestingLevel the nesting level of the array
+     */
+    private static void printArray(final Object[] arr, final int nestingLevel) {
         printTabs(nestingLevel - 1);
         print("[");
 
@@ -39,7 +71,14 @@ public class PrintFormatting {
         print("]");
     }
 
-    private static void print(Object o, int nestingLevel) {
+    /**
+     * Prints the given object to standard output.
+     * The object is indented with a number of tabs equal to the nesting level.
+     *
+     * @param o            the object to print
+     * @param nestingLevel the nesting level of the object
+     */
+    private static void print(final Object o, final int nestingLevel) {
         if (o instanceof int[]) {
             int[] x = ((int[]) o);
             Object[] objs = new Object[x.length];
